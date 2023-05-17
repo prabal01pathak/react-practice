698a119f24f3045e9d67af86857fb90895a57534
Prabal Pathak <prabal01pathak@gmail.com>
2023-05-18 00:21:53 +0530

    added types for user.tsx

--- README.md
+++ README.md
@@ -1,70 +1,28 @@
-# Getting Started with Create React App
+# React Login
+This project is a React.js web application built with TypeScript, Vite and Redux Toolkit that provides a login functionality.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Features
+-  User authentication (sign in, sign up, sign out)
+-  User profile management (update, delete, get)
 
-## Available Scripts
+# Dependencies
+- React.js
+- TypeScript
+- Vite
+- Redux Toolkit
 
-In the project directory, you can run:
+# Getting Started
+- Clone the project repository
+- Install dependencies using npm install
+- Run the app in development mode using npm run dev
+- Open localhost:3000 in your web browser to view the app
+Usage
+- On the login page, enter your credentials and click the "Sign In" button to sign in
+- Click the "Sign Up" button to create a new user account
+- Once signed in, you can view your profile information, update your profile, or delete your account
 
-### `npm start`
+# API
+- The backend API is built with Node.js and Express.js and is not included in this repository. However, the API endpoint URLs are defined in the apiConfig.ts file located in the src/misc directory.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+# License
+This project is licensed under the `MIT License`. See the LICENSE file for details.
--- src/api/user/User.tsx
+++ src/api/user/User.tsx
@@ -1,6 +1,6 @@
 import { userApiUrls } from "../../misc/apiConfig";
 
-export const updateProgressBarAction = (progress) => {
+export const updateProgressBarAction = (progress: any) => {
   // console.log("updateProgressBarAction", progress);
   return {
     type: "UPDATE_PROGRESS_BAR",
@@ -8,48 +8,37 @@ export const updateProgressBarAction = (progress) => {
   };
 };
 
-export async function signIn({ username, password }, thunkAPI) {
+export async function signIn(
+  { username, password }: any,
+
+  thunkAPI: { rejectWithValue: (arg0: any) => any }
+): Promise<any> {
   //   const dispatch = useDispatch();
   const response = await fetch(userApiUrls.signIn, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password }),
-    onProgress: (event) => {
-      if (event.lengthComputable) {
-        console.log("progress: ", event.loaded, event.total);
-        // Calculate the percentage of the download
-        // const progress = Math.round((event.loaded / event.total) * 100);
-        // Dispatch an action to update the progress bar
-        // dispatch(updateProgressBarAction(progress));
-      }
-    },
   });
   try {
     // console.log("creating the user: ", response);
     return await response.json();
-  } catch (error) {
+  } catch (error: any) {
     return thunkAPI.rejectWithValue(error.response.data);
   }
 }
 
-export async function signUp(userData, thunkapi) {
+export async function signUp(
+  userData: Object,
+  thunkapi: { rejectWithValue: (arg0: string) => any }
+) {
   //   const dispatch = usedispatch();
   const response = await fetch(userApiUrls.signUp, {
     method: "POST",
     headers: { "content-type": "application/json" },
     body: JSON.stringify(userData),
-    onprogress: (event) => {
-      if (event.lengthcomputable) {
-        console.log("progress: ", event.loaded, event.total);
-        // calculate the percentage of the download
-        // const progress = math.round((event.loaded / event.total) * 100);
-        // dispatch an action to update the progress bar
-        // dispatch(updateprogressbaraction(progress));
-      }
-    },
   });
   // console.log("creating the user: ", response);
-  if ([200, 201].contains(response.status)) return await response.json();
+  if ([200, 201].includes(response.status)) return await response.json();
   if (response.status >= 400 && response.status <= 499)
     return thunkapi.rejectWithValue(await response.json());
   //if response is in range of 500-599 return something went wrong error
@@ -57,97 +46,72 @@ export async function signUp(userData, thunkapi) {
     return thunkapi.rejectWithValue("Something went wrong");
 }
 
-export async function signOut(thunkapi) {
+export async function signOut(thunkapi: {
+  rejectwithvalue: (arg0: any) => any;
+}) {
   //   const dispatch = usedispatch();
   const response = await fetch(userApiUrls.signOut, {
     method: "GET",
     headers: { "content-type": "application/json" },
     // body: JSON.stringify({ username, password }),
-    onprogress: (event) => {
-      if (event.lengthcomputable) {
-        console.log("progress: ", event.loaded, event.total);
-        // calculate the percentage of the download
-        // const progress = math.round((event.loaded / event.total) * 100);
-        // dispatch an action to update the progress bar
-        // dispatch(updateprogressbaraction(progress));
-      }
-    },
   });
   try {
     console.log("creating the user: ", response);
     return await response.json();
-  } catch (error) {
+  } catch (error: any) {
     return thunkapi.rejectwithvalue(error.response.data);
   }
 }
 
-export async function deleteUser({ username }, thunkapi) {
+export async function deleteUser(
+  { username }: any,
+  thunkapi: { rejectwithvalue: (arg0: any) => any }
+) {
   //   const dispatch = usedispatch();
   console.log("Deleting user: ");
   const response = await fetch(userApiUrls.deleteUser(username), {
     method: "DELETE",
     headers: { "content-type": "application/json" },
     body: JSON.stringify({ username }),
-    onprogress: (event) => {
-      if (event.lengthcomputable) {
-        console.log("progress: ", event.loaded, event.total);
-        // calculate the percentage of the download
-        // const progress = math.round((event.loaded / event.total) * 100);
-        // dispatch an action to update the progress bar
-        // dispatch(updateprogressbaraction(progress));
-      }
-    },
   });
   try {
     return await response.json();
-  } catch (error) {
+  } catch (error: any) {
     return thunkapi.rejectwithvalue(error.response.data);
   }
 }
 
-export async function updateUser(data, thunkapi) {
+export async function updateUser(
+  data: any,
+  thunkapi: { rejectwithvalue: (arg0: any) => any }
+) {
   //   const dispatch = usedispatch();
   const response = await fetch(userApiUrls.updateUser(data.username), {
     method: "PUT",
     headers: { "content-type": "application/json" },
     body: JSON.stringify(data),
-    onprogress: (event) => {
-      if (event.lengthcomputable) {
-        console.log("progress: ", event.loaded, event.total);
-        // calculate the percentage of the download
-        // const progress = math.round((event.loaded / event.total) * 100);
-        // dispatch an action to update the progress bar
-        // dispatch(updateprogressbaraction(progress));
-      }
-    },
   });
   try {
     console.log("Updating user: ", response);
     return await response.json();
-  } catch (error) {
+  } catch (error: any) {
     return thunkapi.rejectwithvalue(error.response.data);
   }
 }
 
-export async function getUser({ username }, thunkapi) {
+export async function getUser(
+  { username }: any,
+  thunkapi: { rejectwithvalue: (arg0: any) => any }
+) {
   //   const dispatch = usedispatch();
   const response = await fetch(userApiUrls.getUser(username), {
     method: "GET",
     headers: { "content-type": "application/json" },
     // body: JSON.stringify(data),
-    onprogress: (event) => {
-      if (event.lengthcomputable) {
-        console.log("progress: ", event.loaded, event.total);
-        // calculate the percentage of the download
-        // const progress = math.round((event.loaded / event.total) * 100);
-        // dispatch an action to update the progress bar
-        // dispatch(updateprogressbaraction(progress));
-      }
-    },
   });
   try {
     return await response.json();
-  } catch (error) {
+  } catch (error: any) {
     return thunkapi.rejectwithvalue(error.response.data);
   }
 }
